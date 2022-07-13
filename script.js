97d44a1c739a681b19f9cf154a450f920b9e4606
emoltz <es5541@nyu.edu>
2022-07-13 09:56:59 -0400

    reformatted

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log("https://github.com/emoltz/connectFour");
+
 const player1 = "Blue";
 const player1Color = 'rgb(86,151,255)';
 
@@ -10,36 +12,42 @@ const player2Color = 'rgb(237,45,73)';
 let game_on = true;
 let table = $('table tr');
 
-function reportWin(rowNum, colNum){
+function reportWin(rowNum, colNum) {
     console.log(`You won starting at ${rowNum}, ${colNum}`);
 }
 
-function changeColor(rowIndex, colIndex, color){
-    if (color===player1Color){
+function changeColor(rowIndex, colIndex, color) {
+    if (color === player1Color) {
         console.log(`${player1} played`);
-    }
-    else{
+    } else {
         console.log(`${player2} played`);
     }
-    return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color',color);
+    return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color', color);
 }
 
-function returnColor(rowIndex, colIndex){
+function returnColor(rowIndex, colIndex) {
     return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
 }
 
-function checkBottom(colIndex){
-    let colorReport;
-    for (let row = 5; row >= 0; row--){
+function checkBottom(colIndex) {
+    let colorReport = "";
+    for (let row = 5; row >= 0; row--) {
         colorReport = returnColor(row, colIndex);
-        if(colorReport === grey){
+        if (colorReport === grey) {
             return row;
         }
     }
 }
 
-console.log(grey);
-function colorMatchCheck(one, two, three, four){
+function colorMatchCheck(one, two, three, four) {
     return (one === two && one === three && one === four && one !== grey && one !== undefined);
 }
 
+function winCheck() {
+    // TODO: logic for winning. After each move, we'll call this function to check if there is a win
+    //horizontal
+
+    //vertical
+
+    //diagonal
+}
